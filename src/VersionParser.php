a63ce1aae03ca18274fb6498977a32b096c3a8bf
Mistralys <s.mordziol@gmail.com>
2023-12-18 15:42:06 +0100

    Improved the parsing, fixed #2.

--- src/VersionParser.php
+++ src/VersionParser.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * File containing the {@see VersionParser} class.
- * 
+ *
  * @package VersionParser
  * @see VersionParser
  */
@@ -10,19 +10,25 @@ declare(strict_types=1);
 
 namespace Mistralys\VersionParser;
 
+use Mistralys\VersionParser\Parser\BuildNumberGenerator;
+use Mistralys\VersionParser\Parser\ComponentDetector;
+use Mistralys\VersionParser\Parser\NumberDetector;
+use Mistralys\VersionParser\Parser\ShortTags;
+use Mistralys\VersionParser\Parser\TagWeights;
+
 /**
  * Version number parsing utility: parses version numbers,
- * and allows retrieving information on the version. 
- * 
+ * and allows retrieving information on the version.
+ *
  * Supports beta, alpha and release candidate tags, as well
- * as custom tags and combinations thereof. 
- * 
+ * as custom tags and combinations thereof.
+ *
  * Expects version to use the following structure:
- * 
+ *
  * <code>Major.Minor.Patch-(Branch or release name)-(Release type: alpha, beta...)</code>
- * 
+ *
  * Examples:
- * 
+ *
  * - 1
  * - 1.0
  * - 1.1.0
@@ -31,13 +37,13 @@ namespace Mistralys\VersionParser;
  * - 1.1.5-beta2
  * - 1.1.5-BranchName
  * - 1.1.5-BranchName-alpha2
- * 
+ *
  * Usage:
- * 
+ *
  * <code>
  * $version = VersionParser::create('1.0');
  * </code>
- * 
+ *
  * @package VersionParser
  * @author Sebastian Mordziol <s.mordziol@mistralys.eu>
  */
@@ -45,43 +51,80 @@ class VersionParser
 {
     public const TAG_TYPE_NONE = 'none';
     public const TAG_TYPE_BETA = 'beta';
+    public const TAG_TYPE_BETA_SHORT = 'b';
     public const TAG_TYPE_ALPHA = 'alpha';
+    public const TAG_TYPE_ALPHA_SHORT = 'a';
     public const TAG_TYPE_RELEASE_CANDIDATE = 'rc';
     public const TAG_TYPE_SNAPSHOT = 'snapshot';
-    
+    public const TAG_TYPE_SNAPSHOT_SHORT = 's';
+    public const TAG_TYPE_DEV = 'dev';
+    public const TAG_TYPE_DEV_SHORT = 'd';
+    public const TAG_TYPE_PATCH = 'patch';
+    public const TAG_TYPE_PATCH_SHORT = 'p';
+    public const TAG_TYPE_STABLE = 'stable';
+
     private string $version;
-    
+
     private float $buildNumber = -1.0;
-    
+
+    private ?VersionTag $tag = null;
+
    /**
     * @var array<int,int>
     */
     private array $parts = array();
-    
-    private string $tagType = self::TAG_TYPE_NONE;
-    private int $tagNumber = 0;
-    private string $branchName = '';
     private string $separator = '-';
     private bool $lowercase = true;
 
-    /**
-    * @var array<string,int>|NULL
-    */
     private static ?array $tagWeights = null;
-    
+
+    private static ?array $shortTags = null;
+
+    private string $original;
+
+    private string $tagParts = '';
+
     private function __construct(string $version)
     {
+        $this->original = $version;
         $this->version = $version;
-        
-        $this->parse();
+
+        $this->parseNumber();
+        $this->parseTag();
     }
 
+    /**
+     * @param bool $uppercase
+     * @return $this
+     * @deprecated Use {@see self::setTagUppercase()} instead.
+     */
     public function setUppercase(bool $uppercase=true) : self
     {
         $this->lowercase = !$uppercase;
         return $this;
     }
 
+    /**
+     * Sets the tag type to be rendered in uppercase
+     * when normalizing the version string, e.g. "1.0-BETA".
+     *
+     * @param bool $uppercase
+     * @return $this
+     */
+    public function setTagUppercase(bool $uppercase=true) : self
+    {
+        $this->lowercase = !$uppercase;
+        return $this;
+    }
+
+    /**
+     * Sets the separator character to use between version
+     * components when normalizing the version string.
+     * Components are the number, tag type and branch name.
+     *
+     * @param string $char
+     * @return $this
+     */
     public function setSeparatorChar(string $char) : self
     {
         $this->separator = $char;
@@ -90,7 +133,7 @@ class VersionParser
 
    /**
     * Creates a new instance for the specified version string.
-    * 
+    *
     * @param string $version
     * @return VersionParser
     */
@@ -101,7 +144,7 @@ class VersionParser
 
    /**
     * Retrieves the version's build number as a float.
-    * 
+    *
     * @return float
     */
     public function getBuildNumber() : float
@@ -110,43 +153,43 @@ class VersionParser
         {
             $this->calculateBuildNumber();
         }
-        
+
         return $this->buildNumber;
     }
 
    /**
     * Retrieves the version's build number as an integer.
-    * 
+    *
     * @return int
     */
     public function getBuildNumberInt() : int
     {
         return (int)($this->getBuildNumber() * 1000000);
     }
-    
+
    /**
     * Retrieves the major version number.
-    * 
+    *
     * @return int
     */
     public function getMajorVersion() : int
     {
         return $this->parts[0];
     }
-    
+
    /**
     * Retrieves the minor version number.
-    * 
+    *
     * @return int
     */
     public function getMinorVersion() : int
     {
         return $this->parts[1];
     }
-    
+
    /**
     * Retrieves the patch version number.
-    * 
+    *
     * @return int
     */
     public function getPatchVersion() : int
@@ -167,14 +210,14 @@ class VersionParser
         {
             return $version;
         }
-        
+
         return $version.$this->separator.$this->getTag();
     }
 
    /**
-    * Retrieves only the numeric version, omitting dots 
+    * Retrieves only the numeric version, omitting dots
     * as far as possible (e.g. `1.0.0` => `1`).
-    * 
+    *
     * @return string
     */
     public function getShortVersion() : string
@@ -191,255 +234,226 @@ class VersionParser
         {
             $keep = array($this->parts[0]);
         }
-        
+
         return implode('.', $keep);
     }
-    
-   /**
+
+    /**
+     * Returns the original version string specified in the constructor.
+     * @return string
+     */
+    public function getOriginalString(): string
+    {
+        return $this->original;
+    }
+
+    /**
     * Retrieves the normalized version tag.
-    * 
+    *
     * @return string
+    * @see self::getTagInfo()
     */
     public function getTag() : string
     {
-        if($this->tagType === self::TAG_TYPE_NONE)
-        {
-            return $this->getBranchName();
+        if(isset($this->tag)) {
+            return $this->tag->getNormalized();
         }
 
-        $tag = $this->tagType;
-
-        if($this->tagNumber > 1)
-        {
-            $tag .= $this->tagNumber;
-        }
-
-        if(!$this->lowercase)
-        {
-            $tag = strtoupper($tag);
-        }
-
-        if($this->hasBranch())
-        {
-            $tag = $this->getBranchName().$this->separator.$tag;
-        }
-
-        return $tag;
+        return '';
     }
-    
+
+    public function getTagInfo() : VersionTag
+    {
+        return $this->tag;
+    }
+
    /**
     * Whether the version has a release tag appended.
-    * 
+    *
     * @return bool
     */
     public function hasTag() : bool
     {
-        return $this->getTag() !== '';
+        return isset($this->tag);
     }
-    
+
    /**
     * Retrieves the type of the release tag.
-    * 
+    *
     * @return string
-    * 
+    *
     * @see VersionParser::TAG_TYPE_NONE
     * @see VersionParser::TAG_TYPE_ALPHA
     * @see VersionParser::TAG_TYPE_BETA
     * @see VersionParser::TAG_TYPE_RELEASE_CANDIDATE
+    * @see VersionParser::TAG_TYPE_DEV
+    * @see VersionParser::TAG_TYPE_SNAPSHOT
+    * @see VersionParser::TAG_TYPE_PATCH
     */
     public function getTagType() : string
     {
-        return $this->tagType;
+        if(isset($this->tag)) {
+            return $this->tag->getTagType();
+        }
+
+        return self::TAG_TYPE_NONE;
     }
-    
+
    /**
     * Retrieves the number of the release tag.
-    * 
+    *
     * @return int The tag number if present, `1` if no number has been specified, and `0` if the version has no tag.
     */
     public function getTagNumber() : int
     {
-        return $this->tagNumber;
+        if(isset($this->tag)) {
+            return $this->tag->getNumber();
+        }
+
+        return 0;
     }
-    
+
     public function isBeta() : bool
     {
-        return $this->getTagType() === self::TAG_TYPE_BETA;
+        if(isset($this->tag)) {
+            return $this->tag->isBeta();
+        }
+
+        return false;
     }
 
     public function isAlpha() : bool
     {
         return $this->getTagType() === self::TAG_TYPE_ALPHA;
     }
-    
+
     public function isReleaseCandidate() : bool
     {
-        return $this->getTagType() === self::TAG_TYPE_RELEASE_CANDIDATE;
+        if(isset($this->tag)) {
+            return $this->tag->isReleaseCandidate();
+        }
+        return false;
     }
 
     public function isSnapshot() : bool
     {
-        return $this->getTagType() === self::TAG_TYPE_SNAPSHOT;
+        if(isset($this->tag)) {
+            return $this->tag->isSnapshot();
+        }
+
+        return false;
     }
-    
+
+    public function isDev() : bool
+    {
+        if(isset($this->tag)) {
+            return $this->tag->isDev();
+        }
+
+        return false;
+    }
+
+    public function isPatch() : bool
+    {
+        if(isset($this->tag)) {
+            return $this->tag->isPatch();
+        }
+
+        return false;
+    }
+
+    public function isStable() : bool
+    {
+        if(isset($this->tag)) {
+            return $this->tag->isStable();
+        }
+
+        // To tag = stable
+        return true;
+    }
+
    /**
     * Whether a branch name is present in the version.
-    * 
+    *
     * @return bool
     */
     public function hasBranch() : bool
     {
-        return !empty($this->branchName);
+        return !empty($this->getBranchName());
     }
-    
+
    /**
     * Retrieves the branch name, if any.
-    * 
+    *
     * @return string The branch name, or an empty string if none.
     */
     public function getBranchName() : string
     {
-        return $this->branchName;
+        if(isset($this->tag)) {
+            return $this->tag->getBranchName();
+        }
+
+        return '';
     }
-    
+
    /**
     * Retrieves the version without tag, normalized to
-    * use all three levels, even if less have been 
+    * use all three levels, even if less have been
     * specified (e.g. `1` => `1.0.0`).
-    * 
+    *
     * @return string
     */
     public function getVersion() : string
     {
         return implode('.', $this->parts);
     }
-    
-    private function parse() : void
-    {
-        $parts = explode('.', $this->extractTag());
-        $parts = array_map('trim', $parts);
-        
-        while(count($parts) < 3)
-        {
-            $parts[] = 0;
-        }
-        
-        for($i=0; $i < 3; $i++)
-        {
-            $this->parts[] = (int)$parts[$i];
-        }
-    }
-    
-    private function extractTag() : string
-    {
-        $version = $this->version;
-        $version = str_replace('_', '-', $version);
-        
-        $hyphen = strpos($version, '-');
-        
-        if($hyphen !== false)
-        {
-            $tag = substr($version, $hyphen+1);
-            $version = substr($version, 0, $hyphen);
-            $this->parseTag($tag);
-        }
 
-        return $version;
-    }
-
-    private function formatTagNumber() : string
+    private function parseTag() : void
     {
-        $tagWeights = self::getTagWeights();
-        $positions = 2 * 3;
-        $weight = $tagWeights[$this->getTagType()];
-        
-        if($weight > 0)
-        {
-            $number = sprintf('%0'.$weight.'d', $this->tagNumber);
-            $number = str_pad($number, $positions, '0');
-
-            $number = (int)str_repeat('9', $positions) - (int)$number;
-            return '.'.$number;
-        }
-        
-        return '';
-    }
-    
-    private function parseTag(string $tag) : void
-    {
-        $parts = explode('-', $tag);
-        
-        foreach($parts as $part)
-        {
-            $this->parseTagPart($part);
-        }
-        
-        if($this->tagNumber === 0)
-        {
-            $this->tagNumber = 1;
-        }
-        
-        if($this->tagType === self::TAG_TYPE_NONE)
-        {
-            $this->tagNumber = 0;
-        }
-    }
-    
-    private function parseTagPart(string $part) : void
-    {
-        if(is_numeric($part))
-        {
-            $this->tagNumber = (int)$part;
+        if (empty($this->tagParts)) {
             return;
         }
-        
-        $types = array_keys(self::getTagWeights());
-        $type = '';
-        $lower = strtolower($part);
-        
-        foreach($types as $tagType)
-        {
-            if(strpos($lower, $tagType) !== false)
-            {
-                $type = $tagType;
-                $part = str_replace($tagType, '', $lower);
-            }
-        }
-        
-        if(empty($type))
-        {
-            if(!empty($part))
-            {
-                $this->branchName = $part;
-            }
-            
-            return;
-        }
-        
-        $this->tagType = $type;
-        
-        if(is_numeric($part))
-        {
-            $this->tagNumber = (int)$part;
-        }
+
+        $this->tag = (new ComponentDetector($this, $this->tagParts))->detectTag();
     }
-    
+
+    public function toArray() : array
+    {
+        $tag = null;
+        if(isset($this->tag)) {
+            $tag = $this->tag->toArray();
+        }
+
+        return array(
+            'originalVersion' => $this->getOriginalString(),
+            'normalized' => $this->getVersion(),
+            'majorVersion' => $this->getMajorVersion(),
+            'minorVersion' => $this->getMinorVersion(),
+            'patchVersion' => $this->getPatchVersion(),
+            'shortVersion' => $this->getShortVersion(),
+            'buildNumber' => $this->getBuildNumber(),
+            'buildNumberInt' => $this->getBuildNumberInt(),
+            'tag' => $tag
+        );
+    }
+
+    private function parseNumber() : void
+    {
+        $number = new NumberDetector($this->version);
+
+        $this->parts = $number->getNumbers();
+        $this->tagParts = $number->getTagString();
+    }
+
     private function calculateBuildNumber() : void
     {
-        $parts = array(
+        $this->buildNumber = (new BuildNumberGenerator(
             $this->getMajorVersion(),
-            sprintf('%03d', $this->getMinorVersion()),
-            sprintf('%03d', $this->getPatchVersion())
-        );
-        
-        $number = (float)implode('', $parts);
-        
-        if($this->tagNumber > 0)
-        {
-            $number -= (float)$this->formatTagNumber();
-        }
-        
-        $this->buildNumber = $number;
+            $this->getMinorVersion(),
+            $this->getPatchVersion(),
+            $this->tag
+        ))
+            ->getBuildNumber();
     }
 
     /**
@@ -477,34 +491,33 @@ class VersionParser
      *              "alpha" has a default weight of "6" and beta a weight of "4".
      * @return void
      */
-    public static function registerTagType(string $name, int $weight) : void
+    public static function registerTagType(string $name, int $weight, string $shortName='') : void
     {
-        $tagWeights = self::getTagWeights();
-        $tagWeights[$name] = $weight;
-
-        self::$tagWeights = $tagWeights;
+        TagWeights::registerTagType($name, $weight, $shortName);
     }
 
     /**
      * @return array<string,int>
+     * @deprecated Use {@see TagWeights::getWeights()} instead.
      */
     public static function getTagWeights() : array
     {
-        if(!isset(self::$tagWeights)) {
-            self::$tagWeights = array(
-                self::TAG_TYPE_ALPHA => 6,
-                self::TAG_TYPE_BETA => 4,
-                self::TAG_TYPE_RELEASE_CANDIDATE => 2,
-                self::TAG_TYPE_SNAPSHOT => 0,
-                self::TAG_TYPE_NONE => 0
-            );
-        }
-
-        return self::$tagWeights;
+        return TagWeights::getWeights();
     }
 
     public static function resetTagTypes() : void
     {
-        self::$tagWeights = null;
+        TagWeights::reset();
+        ShortTags::reset();
+    }
+
+    public function isTagUppercase() : bool
+    {
+        return !$this->lowercase;
+    }
+
+    public function getSeparatorChar() : string
+    {
+        return $this->separator;
     }
 }
